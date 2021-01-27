fd7d07036c8e3b97ba1f46a65e8b6ee192ae829b
farahm20 <mahboob.farah@gmail.com>
2021-01-27 23:33:47 +0100

    Fixed About.vue.

--- Frontend/src/views/About.vue
+++ Frontend/src/views/About.vue
@@ -1,12 +1,16 @@
 <template>
   <section>
       <h1>This is about</h1>
+      <AboutText />
   </section>
 </template>
 
 <script>
+import AboutText from './../components/About/AboutText'
 export default {
-
+  components: {
+    AboutText
+  }
 }
 </script>
 
