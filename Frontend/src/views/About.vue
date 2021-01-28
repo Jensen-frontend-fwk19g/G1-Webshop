ab1b3954cdba4fabdf91b91b95abbd74234721d9
sanazshamsi <sanaz.shamsipour84@gmail.com>
2021-01-28 09:47:30 +0100

    about page

--- Frontend/src/views/About.vue
+++ Frontend/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Dear readers</h1>
+    <p>We offer tremendous gathering of books in various classification of Fiction, 
+      Non-fiction, Biographies, History, Religions, Self – Help, Children. We likewise 
+      move in immense accumulation of Investments and Management, Computers, Engineering, 
+      Medical, College and School content references books proposed by various foundations 
+      as schedule the nation over. Other than to this, we likewise offer an expansive 
+      gathering of E-Books at reasonable valuing.
+    </p>
   </div>
 </template>
+<style scoped>
+.about {
+  text-align: justify;
+  margin: 10px 40px;
+}
+</style>
