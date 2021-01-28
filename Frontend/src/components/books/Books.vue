636889d1ddc22b67b657f34a0fa735a846c5b08d
farahm20 <mahboob.farah@gmail.com>
2021-01-28 09:51:09 +0100

    added missing div

--- Frontend/src/components/books/Books.vue
+++ Frontend/src/components/books/Books.vue
@@ -4,42 +4,41 @@
     <div v-for="book in allBooks" :key='book.id' @click="goTo(book.id)" class="books" >
         <h2>{{book.Title}}</h2>
         <h3>{{book.Price}}</h3>
+       </div> 
+        </div> 
 </template>
 
 <script>
 export default {
-    data: () => ({
-        allBooks: Array,
-    }),
-    methods: {
-        async fetchBooks() {
-            try {
-                const response = await fetch('http://localhost:3000/products');
-                const json = await response.json();
-                this.allBooks = json.products;
-                this.$emit('allBooks', this.allBooks)
-            }
-            catch {
-                this.allBooks = 'Could not retrive all books, please try again.'
-            }
-        },
-        goTo(id){
-            this.$router.push(`/BookInfo/${id}`)
-            console.log(id)
-        }
+  data: () => ({
+    allBooks: Array,
+  }),
+  methods: {
+    async fetchBooks() {
+      try {
+        const response = await fetch("http://localhost:3000/products");
+        const json = await response.json();
+        this.allBooks = json.products;
+        this.$emit("allBooks", this.allBooks);
+      } catch {
+        this.allBooks = "Could not retrive all books, please try again.";
+      }
     },
-    beforeMount() {
-        this.fetchBooks()
-    }
-
-}
+    goTo(id) {
+      this.$router.push(`/BookInfo/${id}`);
+      console.log(id);
+    },
+  },
+  beforeMount() {
+    this.fetchBooks();
+  },
+};
 </script>
 
 <style>
 .books {
-    background: white;
-    width: 10em;
-    height: 6em;
+  background: white;
+  width: 10em;
+  height: 6em;
 }
-
 </style>
