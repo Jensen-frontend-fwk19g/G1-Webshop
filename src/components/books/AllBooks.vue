55b4d812d762f83bdca2de9cca4ef85e17d88a13
mariaahlen <59694731+mariaahlen@users.noreply.github.com>
2021-02-02 13:59:03 +0100

    Revert "Merge branch 'sanaz' into dev"
    This reverts commit c6d3dece01499b70cbf6b021cef09437904dcf2e, reversing
    changes made to ebf4935385f33ec9a597391c11b2bf4abbefa8e2.

--- src/components/books/AllBooks.vue
+++ src/components/books/AllBooks.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="books" >
-        <h2 @click="goTo(book.id)" >{{book.Title}}</h2>
-        <h3>{{book.Price}} kr</h3>
-        <img :src="'/img/'+ book.Img" alt="bild">
-        <button @click="addToCart(book.id)">add to cart</button>        
+    <div @click="goTo(book.id)" class="books" >
+        <h2>{{book.Title}}</h2>
+        <h4>Type: {{book.Type}}</h4>
+        <h3>{{book.Price}} :-</h3>
+        <img :src="book.Img" alt="bild">        
     </div>
 </template>
 
 <script>
-
 export default {
   props: {
     book: Object,
@@ -17,48 +16,8 @@ export default {
     goTo(id) {
       this.$router.push(`/BookInfo/${id}`);
     },
-    data() {
-        return {
-            cart:[]
-        }
-    },
-    methods: {
-        goTo(id){
-            this.$router.push(`/BookInfo/${id}`)
-        },
-        addToCart(item) {
-
-            /*this.cartadd.id = item.id
-            this.cartadd.Title = item.Title
-            this.cartadd.Price = item.Price
-            this.cartadd.Type = item.Type
-            this.cart.push(this.cartadd)
-            this.cartadd = {}
-            console.log(item)
-            let cartItem = this.cart.forEach(book => book.id == item.id);
-            console.log(cartItem,'kkkkkk')
-
-            if (cartItem) {
-                cartItem.quantity++
-            } 
-            else
-            {
-                //else add to cart with quantity 1
-                //product.quantity = 1;
-                this.cart.push({
-                id: item.id,
-                price: item.Price,
-                title: item.Title,
-                quantity: 1
-                });
-            }*/
-            this.cart.push(item);
-            console.log(this.card,'ooo')
-            
-        }
-    }
-}
-}
+  },
+};
 </script>
 
 <style scoped>
